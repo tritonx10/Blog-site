69ed6a8fabda554559379a253c3e5160c7174777
tritonx10 <rahulmahat1010@gmail.com>
2026-03-20 21:17:55 +0530

    v2

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
